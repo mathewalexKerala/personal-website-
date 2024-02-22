8fe99b2a63e38fba519da27fddcc33805571babd
JISHNU T RAJ <jtrtricks@gmail.com>
2024-02-22 11:30:34 +0530

    style:[updated] some minor things

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Projects from "./components/Projects/Projects";
 import ErrorPage from "./components/ErrorPage/ErrorPage";
+import Works from "./components/Works/Works";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route  path="/work" element={<Works />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,9 @@
-import { useState } from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <p>jishnu t raj.</p>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="lucide lucide-menu menu"
-      >
-        <line x1="4" x2="20" y1="12" y2="12" />
-        <line x1="4" x2="20" y1="6" y2="6" />
-        <line x1="4" x2="20" y1="18" y2="18" />
-      </svg>
-      <ul>
-        <li>home.</li>
-        <li>work.</li>
-        <li>contact.</li>
-      </ul>
     </nav>
   );
 };
